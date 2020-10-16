8aba30c1d368297d0579d881cfa8055ac4902b83
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 12:08:05 -0500

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,21 @@
+'''
+-----------------------------
+ EJERCICIO N°1
+ La función input()
+-----------------------------
+En este programa:
+ 1. Se solicita al usuario que ingrese algún dato en la consola
+ 2. input() pone la consola en modo de entrada
+ 3. Los datos se guardan en la variable “algo” y se envían al programa
+ 4. print() muestra los datos obtenidos
+ -----------------------------
+'''
+print("Dime algo...")
+algo = input()
+print("Mmm...", algo, "...¿en serio?")
+'''
+-----------------------------
+5. Simplifica el código utilizando argumentos en la función input()
+-> algo = input("Mensaje")
+-----------------------------
+'''
