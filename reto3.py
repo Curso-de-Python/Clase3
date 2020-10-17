467044aa6f67bec0285eb164fa2f899f8f653f59
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 14:01:21 -0500

    Create reto3.py

---
+++ reto3.py
@@ -0,0 +1,14 @@
+'''
+-----------------------------
+ RETO N°3
+ Operadores y expresiones
+ -----------------------------
+ Prepara un código para encontrar el tiempo final de un periodo de tiempo dado, expresándolo en horas y minutos. Las horas van de 0 a 23 y los minutos de 0 a 59. 
+
+ Por ejemplo, si el evento comienza a las 12:17 y dura 59 minutos, terminará a las 13:16.
+ *PISTA: El operador % puede ser enormemente útil.
+-----------------------------
+'''
+hora = int(input("Hora de inicio (horas): "))
+minuto = int(input("Minuto de inicio (minutos): "))
+duracion = int(input("Duración del evento (minutos): "))
