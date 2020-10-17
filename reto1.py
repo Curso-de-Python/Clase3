5130802b8e7551b34985bc77fd559e597d980e3c
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 10:11:28 -0500

    Create reto1.py

---
+++ reto1.py
@@ -0,0 +1,16 @@
+'''
+-----------------------------
+ RETO N°1
+ El teorema de Pitágoras
+-----------------------------
+ "La longitud de la hipotenusa es igual a la raíz cuadrada de la suma de los cuadrados de los catetos"
+
+El programa debería funcionar de la siguiente manera:
+ 1. Se le pregunta al usuario la longitud de los dos catetos
+ 2. Guarda estos valores en variables
+ 3. Calcula la hipotenusa
+ 4. Imprime el resultado
+ *Nota: print() debe contener solo un argumento de cadena, sin comas
+-----------------------------
+'''
+cateto_a = float(input("Ingrese el valor del primer cateto: "))
