2be4bc462a0641f740f3f377ca8361424c4ecf80
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-17 14:01:05 -0500

    Create reto2.py

---
+++ reto2.py
@@ -0,0 +1,14 @@
+'''
+-----------------------------
+ RETO N°2
+ Declaraciones condicionales
+-----------------------------
+ El espatifilo es una planta interior popular que filtra las toxinas dañinas del aire.
+ Imagina que tu programa ama estas plantas. Cada vez que recibe la palabra Espatifilo, grita a la consola:  "¡Espatifilo es la mejor planta de todas!" 
+
+Escribe un programa que tome una cadena como entrada e imprima:
+ - "Si, ¡El Espatifilo es la mejor planta de todos los tiempos!" si se ingresa "Espatifilo".
+ - "No, ¡quiero un gran Espatifilo!" si se ingresa "espatifilo".
+ - "¡Espatifilo! ¡No [entrada]!" si se ingresa cualquier otra palabra. Nota: [entrada] es la cadena de entrada.
+-----------------------------
+'''
