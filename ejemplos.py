2ac8f810088968a011b7cd2d779edf01a54988ee
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-30 15:42:40 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,32 @@
+# OPERADORES DE CADENAS
+x = int(input("Ingresa un número: "))
+print(x * "5")
+
+#x = int(input("Ingresa un número: "))
+#print(type(x))
+
+# -------------------------
+# CONVERSIÓN DE DATOS STR()
+#edad = input("¿Cuántos años tienes?: ")
+#print("¡Vaya! No pareces de" + str(edad))
+
+# -------------------------
+# COMPARACIÓN
+#var = 0
+#print(var == 0)
+#print(var != 0)
+
+# -------------------------
+# CONDICIONAL IF-ELSE
+'''
+x = 10
+
+if x == 10:
+  print(x == 10)
+if x > 5:
+  print(x > 5)
+if x < 10:
+  print(x < 10)
+else: 
+  print("else")
+'''
